15a9327ed079c34eb0a79ae463efd74235ada15e
i1caro <rnunojanela@gmail.com>
2014-03-27 14:45:36 +0000

    Update mirror-site.py

--- mirror-site.py
+++ mirror-site.py
@@ -44,6 +44,45 @@ def merge_files(site):
           os.rename(old_path, new_path)
 
 
+def to_relative(directorypath, match):
+  split_url = match.split('/')
+  split_path = directorypath.split('/')[1:]
+  number_of_similar = 0
+  for path_dir, path_url in zip(split_path, split_url):
+    if path_dir != path_url:
+      break
+    number_of_similar += 1
+
+  result = '/'.join(split_url[number_of_similar:])
+  different_paths = len(split_path) - number_of_similar
+  if different_paths > 0:
+    return '/'.join(['..' for o in range(different_paths)]) + '/' + result
+  return result
+
+
+def turn_full_path_links_into_relative(site):
+  regex_url = re.compile('[\'\"](https?\:\/\/{}\/)(.+?)[\'\"]'.format(site))
+  for directorypath, directoryname, files in os.walk(site):
+    for filename in files:
+      current_file = os.path.join(directorypath, filename)
+      with open(current_file, 'r') as opened_file:
+        content = opened_file.read()
+
+      with open(current_file, 'w') as opened_file:
+        matches = regex_url.findall(content)
+        if not matches:
+          continue
+        matches = set(matches)
+        print directorypath + '/' + filename
+        for match_site, match in matches:
+          relative_url = to_relative(directorypath, match)
+          absolute_url = match_site + match
+          print ' .... %s' % absolute_url
+          print ' .... %s' % relative_url
+          content = content.replace(absolute_url, relative_url)
+        opened_file.write(content)
+
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Mirrors website and merges the GET parameters into a single file needs wget to be installed.')
   parser.add_argument('site', metavar='Url', type=str, nargs='?', help='Site url or path')
@@ -56,4 +95,7 @@ if __name__ == '__main__':
     print 'Mirroring {}'.format(site)
     mirror(site)
     print 'Merging files'
-  merge_files(site)
+  # merge_files(site)
+  turn_full_path_links_into_relative(site)
+
+
